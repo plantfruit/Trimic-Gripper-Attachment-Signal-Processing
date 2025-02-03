9261ea7e6399a4c54fb3f546ae93f0c87df38394
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2025-02-03 17:49:52 -0500

    Miscellaneous Object Classification
    6 objects

--- LabelCreator.m
+++ LabelCreator.m
@@ -2,7 +2,7 @@
 
 pulseNum = 10; % Number of pulses extracted from each file
 fileNum = 10; % Number of trials conducted for each label
-gridSize = 9; % Number of labels, or points in the grid. E.g. If it's a 5x5 grid, we have 25 points
+gridSize = 6; % Number of labels, or points in the grid. E.g. If it's a 5x5 grid, we have 25 points
 
 labels = zeros(gridSize * fileNum * pulseNum, 1);
 
@@ -13,4 +13,4 @@ for i = 1:gridSize
 end
 
 
-save("3x3_labels.txt", "labels", "-ascii")
+save("miscobj3_labels.txt", "labels", "-ascii")
