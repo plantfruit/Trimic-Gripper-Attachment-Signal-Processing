1278ce45ab6f7f8335583dbb65a311386d426076
Kevin Xu <60991949+plantfruit@users.noreply.github.com>
2025-02-13 20:47:21 -0500

    String Labels
    For the 6 objects

--- LabelCreator.m
+++ LabelCreator.m
@@ -4,12 +4,14 @@ pulseNum = 10; % Number of pulses extracted from each file
 fileNum = 10; % Number of trials conducted for each label
 gridSize = 6; % Number of labels, or points in the grid. E.g. If it's a 5x5 grid, we have 25 points
 
-labels = zeros(gridSize * fileNum * pulseNum, 1);
+labels = strings(gridSize * fileNum * pulseNum, 1);
+
+objNames = {"Stylus", "Screwdriver", "Battery", "Plug", "Motor", "Tripod"};
 
 for i = 1:gridSize
     for j = 1:fileNum * pulseNum
-        labels((i - 1) * fileNum * pulseNum + j) = i;
+        labels((i - 1) * fileNum * pulseNum + j) = i; %objNames{i};
     end
 end
 
-save("1Dtube_6obj_labels.txt", "labels", "-ascii")
+save("1Dtube_6obj2_labels.txt", "labels", "-ascii")
